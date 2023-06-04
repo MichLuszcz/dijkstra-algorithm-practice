7cbc200517cc7d083fde69513e7a39c11b8b94b6
Karol Kuc <01178549@pw.edu.pl>
2023-06-04 18:32:17 +0200

    Modified the way of reading board

--- opening_a_file.py
+++ opening_a_file.py
@@ -4,17 +4,25 @@ from CellClass import Cell
 
 def prep_parser() -> str:
     parser = ArgumentParser()
-    parser.add_argument("filename", help="Name of a file to read")
     args = parser.parse_args()
+    parser.add_argument("filename", help="Name of a file to read")
+
     return args.filename
 
 
 def read_board() -> list:
     filename = prep_parser()
     board = []
+    len_of_row = 0
+    diff = 0
     with open(filename, 'r') as file_handle:
-        for line in file_handle:
-            board.append(list(line.strip()))
+        for index, line in enumerate(file_handle):
+            board.append(list(line.rstrip('\n')))
+            if len(line.rstrip('\n')) < len_of_row:
+                diff = len_of_row - len(line.rstrip('\n'))
+                for _ in range(diff):
+                    board[index].append(' ')
+            len_of_row = len(board[index])
 
     return board
 
@@ -25,25 +33,26 @@ def number_to_object_grid(number_grid: list) -> list:
     for row in number_grid:
         object_row = []
         column_index = 0
-        for number in row:
-            if number == '0':  # ścieżka
-                object_row.append(Cell(float('inf'), True, [column_index, row_index]))
-            elif number == '1':  # ściana
-                object_row.append(Cell(float('inf'), False, [column_index, row_index]))
-            elif number == '2':  # drzwi
-                object_row.append(Cell(float('inf'), False, [column_index, row_index], 'nn'))
+        for element in row:
+            if element.isnumeric():
+                object_row.append(Cell(float('inf'), True, [column_index, row_index], element))
+            elif element == 'X' or element == 'x':
+                object_row.append(Cell(float('inf'), True, [column_index, row_index], 0))
             else:
-                object_row.append(Cell(float('inf'), False, [column_index, row_index], number))
+                # spacja
+                object_row.append(Cell(float('inf'), False, [column_index, row_index], 0))
             column_index += 1
         object_grid.append(object_row)
         row_index += 1
     return object_grid
 
 
-# def test():
-#     board = read_board()
-#     obj_board = number_to_object_grid(board)
-#     for row in obj_board:
-#         print(row)
+def test():
+    board = read_board()
+    for row in board:
+        print(row)
+    obj_board = number_to_object_grid(board)
+    for row in obj_board:
+        print(row)
 
-# test()
+test()
