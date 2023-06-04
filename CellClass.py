85cd552225a3d7a654cdb93ee11988a7afda4e47
Michał Łuszczek <michluszcz@gmail.com>
2023-06-04 20:00:40 +0200

    adjusted the Cell class to include a weight

--- CellClass.py
+++ CellClass.py
@@ -1,9 +1,10 @@
 class Cell:
-    def __init__(self, distance, is_walkable, coordinates, room_text=None):
+    def __init__(self, distance, is_walkable, coordinates, weight):
         self.distance = distance
         self.is_walkable = is_walkable
         self.coordinates = coordinates
-        self.room_text = room_text
+        self.parent_coordinates = None
+        self.weight = weight
 
     def __repr__(self) -> str:
         return "Cell coords(x:" + str(self.coordinates[0]) + " y:" + str(self.coordinates[1]) + ")"
