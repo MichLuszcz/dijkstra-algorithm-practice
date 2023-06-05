d10e02e5b4654b82a03101c4aed120c8e687e8a1
Michał Łuszczek <michluszcz@gmail.com>
2023-06-05 18:12:32 +0200

    implemented finind the begninng and end and printing the X

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def show_result(obj_board: list, path: list):
         for element in row:
             if element.isnumeric():
                 result += element
+            elif element == "-1":
+                result += "X"
             else:
                 result += " "
         result += '\n'
@@ -26,11 +28,22 @@ def show_result(obj_board: list, path: list):
     print(result)
 
 
+def find_start_and_finish(grid: list(list())):
+    findings = []
+    for row in grid:
+        for cell in row:
+            if cell.weight == -1:
+                findings.append(cell.coordinates)
+    return findings
+
+
 def main():
     board = read_board()
     obj_board = number_to_object_grid(board)
-    path = find_path_dijkstra([1, 1], [2, 4], obj_board)
+    start_finish = find_start_and_finish(obj_board)
+    path = find_path_dijkstra(start_finish[0], start_finish[1], obj_board)
     show_result(obj_board, path)
+    print(board)
 
 
 if __name__ == "__main__":
